9d4c2f23693ab0d4a5eb2e648644226a4f6a8ea6
KillByte <killbyte.prototype@gmail.com>
2021-01-01 17:40:40 +0000

    General commit

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const Student = require('../controllers/user')
+const User = require('../controllers/user')
 
 const router = express.Router();
 
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/students', function(req, res) {
   // Data retrieve
-  Student.list()
-    .then(data => res.render('students', { list: data }))
-    .catch(err => res.render('error', {error: err}))
-  ;
+  User.list()
+    .then(data => {console.log("ok1"); res.render('index', { title: 'Ola mundo cruel' }); console.log("ok2");})
+    .catch(err => {console.log("ok3"); res.render('error', { error: err }); console.log("ok4"); });
+
 });
 
 module.exports = router;
