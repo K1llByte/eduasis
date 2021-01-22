1ece118521a5b321c699b548c5bebc53ccdb0ef1
KillByte <killbyte.prototype@gmail.com>
2021-01-22 19:38:21 +0000

    Added User Authentication

--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -1,69 +1,18 @@
 const express = require('express');
 const User = require('../controllers/user');
+const auth = require('../controllers/auth');
 
 const router = express.Router();
 
-// ========= AUTH ========= //
-
-// TODO: PLACE ALL FORM OF CODE VALIDATION AND TOKEN GENERATION ON A SEPARATED MODULE
-function validate_auth(required_permission = undefined)
-{
-    
-    if(required_permission == undefined)
-        return (req, res, next) => {
-            if(req.query.token /* == undefined */ )
-            {
-                console.log("Query Param Token:",req.query.token);
-
-                jwt.verify(req.query.token,'DAW2020', (e,payload) => {
-                    if(e /* == undefined */)
-                    {
-                        // 401 Unauthorized
-                        res.status(401).jsonp({ error : e });
-                    }
-                    else
-                    {
-                        if(required_permission == undefined)
-                        {
-                            req.user = { 
-                                perms : payload.perms,
-                                username : payload.username
-                            };
-                            next();
-                        }
-                        else if(payload.perms >= required_permission)
-                        {
-                            req.user = { 
-                                perms : payload.perms,
-                                username : payload.username
-                            };
-                            next();
-                        }
-                        else
-                        {
-                            // 403 Forbidden
-                            res.status(403).jsonp({error: 'Forbidden! Insufficient permissions'});
-                        }
-                    }
-                });
-            }
-            else
-            {
-                // 401  Unauthorized
-                res.status(401).jsonp({error: 'Invalid token or non-existing'});
-            }
-        };
-    else
-        return (req, res, next) => {
-            
-        };
-    
-    // // req, res, next
-    
-}
-
 // ========= ROUTES ========= //
 
+
+router.get('/api/gen_token', (req, res) => {
+
+    const token = auth.gen_token({ username:"user1" , perms:1 });
+    res.json({ "TOKEN":token });
+    
+});
 
 
 router.get('/api/users', (req, res) => {
@@ -78,7 +27,7 @@ router.get('/api/users', (req, res) => {
 });
 
 
-router.get('/api/users/:username', (req, res) => {
+router.get('/api/users/:username', auth.validate_auth(1), (req, res) => {
 
     User.get(req.params.username)
         .then(data => { 
@@ -88,6 +37,5 @@ router.get('/api/users/:username', (req, res) => {
             res.json('error', err);
         });
 });
-
 
 module.exports = router;
