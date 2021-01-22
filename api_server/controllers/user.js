f8e5356b46d1ea55d16d1a5b21467b55e152057b
KillByte <killbyte.prototype@gmail.com>
2021-01-22 16:16:54 +0000

    General commit

--- api_server/controllers/user.js
+++ api_server/controllers/user.js
@@ -1,13 +1,8 @@
 const User = require('../models/user');
 
-// TODO OPERATIONS:
-// list_all()
-// add(user_data)
-// get(username)
-// set(user_data)
-// delete(user_data)
+// ===== CRUD Operations ===== //
 
-// Returns student list
+// Returns all users list
 module.exports.list_all = () => {
     return User
         .find({},{ _id:0, password_hash:0})
@@ -15,30 +10,31 @@ module.exports.list_all = () => {
         .exec();
 }
 
+// Returns a user by username
 module.exports.get = (uname) => {
     return User
         .findOne({ username: uname })
         .exec();
 }
 
+// Updated user data
 module.exports.set = (userdata) => {
     return User
         .updateOne({username:userdata['username']},{$set : data})
         .exec();
 }
 
+// Adds a new user data
 module.exports.add = (userdata) => {
     const new_user = new User(userdata)
     return new_user.save();
 }
 
+// Deletes user by username
 module.exports.delete = (uname) => {
     return User
         .deleteOne({ username: uname })
         .exec();
 }
 
-// module.exports.insert = student => {
-//     var new_user = new User(student)
-//     return newStudent.save()
-// }
+// =========================== //
--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -3,13 +3,62 @@ const User = require('../controllers/user');
 
 const router = express.Router();
 
+// ========= AUTH ========= //
+
+function validate_auth(required_permission = undefined)
+{
+    if(required_permission == undefined)
+        return (req, res, next) => {};
+    else
+        return (req, res, next) => {};
+    
+    // // req, res, next
+    // if(req.query.token)
+    // {
+    //     console.log("Bearer Token:",req.query.token);
+    //     jwt.verify(req.query.token,'DAW2020', (e,payload) => {
+    //         if(e)
+    //         {
+    //             res.status(401).jsonp({ error : e });
+    //         }
+    //         else
+    //         {
+    //             req.user = { 
+    //                 lvl : payload.lvl,
+    //                 username : payload.username
+    //             };
+    //             next();
+    //         }
+    //     });
+    //     //// 401 Unauthorized
+    //     //next();
+    // }
+    // else
+    // {
+    //     console.log("erro1");
+    //     res.status(401).jsonp({error: 'Invalid token or non-existing'});
+    // }
+}
+
 // ========= ROUTES ========= //
 
 
 
-router.get('/api/users', function(req, res) {
-    // Data retrieve
+router.get('/api/users', (req, res) => {
+    
     User.list_all()
+        .then(data => { 
+            res.json(data);
+        })
+        .catch(err => { 
+            res.json('error', err);
+        });
+});
+
+
+router.get('/api/users/:username', (req, res) => {
+
+    User.get(req.params.username)
         .then(data => { 
             res.json(data);
         })
