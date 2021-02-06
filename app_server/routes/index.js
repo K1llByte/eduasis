b6afdec738f99090e45d0221859ed933a5ab2279
cati-123-199 <catarinafreitascruz@gmail.com>
2021-02-06 23:56:51 +0000

    home_page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -441,8 +441,7 @@ router.get('/resources/:resource_id', check_auth, (req, res) => {
 //POST rate resource
 router.post('/resources/:resource_id/rate', check_auth, (req, res) => {
     // Data retrieve
-    console.log("VALUE",req.body);
-
+    // console.log("VALUE",req.body);
     axios.put(`${API_URL}/resources/${req.params.resource_id}/rate`, {value: req.body.value}, auth_header(req.user.token))
     .then(res.redirect('/resources/'+req.params.resource_id))
     .catch(err => res.render('error', {err:err}));
